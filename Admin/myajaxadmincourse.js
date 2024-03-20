d54576f7b2d600a73f241e7a85a20b959429015c
Bendre Pratiksha <bendrepratiksha747@gmail.com>
2024-03-20 18:00:33 +0530

    course

--- Admin/myajaxadmincourse.js
+++ Admin/myajaxadmincourse.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+    
     //show_Data();
     function show_Data()
     {
@@ -164,16 +164,7 @@ function myfunc3()
                     output+='<div class="col-xl-3 col-md-6 col-12">';
                     output+='<div class="d-flex justify-content-center align-items-center">';
                     output+='<div class="card mt-3" style="width: 18rem;>';
-                    output+='<div class="card-body">';
-                    output+='<div class="d-flex justify-content-between p-3">';
-                    output+='<p class="small text-grey "><i class="fa-solid fa-book-open-reader"></i></p>';
-                    output+='<p class="small text-grey">'+x[i].ac_year+'</p>';
-                    output+='</div>';
-                    output+='<h5 class="card-title" style="margin-left:1pc;">'+x[i].subject_name+'</h5>';
-                    output+='<p class="card-text  mb-4" style="margin-left:1pc;">';
-                    output+=''+x[i].allocated_faculty+'</p>';
-                    output+='</div>';
-                    output+='</div>';
+                    
                     output+='</div>';
                     output+='</div>';
                     
